554cb6c63bb27dab23fcfe4d54a366d36cbfd210
Nuruddin <sg7720@gmail.com>
2020-12-02 17:24:48 +0300

    add background images

--- src/Data.js
+++ src/Data.js
@@ -2,6 +2,8 @@ import fes from "./img/fes.jpg"
 import tanger from "./img/tanger.jpg"
 import kairun from "./img/kairun.jpg"
 import alkar from "./img/alkar.jpg"
+import madina from "./img/madina.jpg"
+import ornament from "./img/ornament.jpg"
 export let levels = []
 let lvls = [{
     mainword: "ملكس",
@@ -11,25 +13,39 @@ let lvls = [{
     img: fes
 },
 {
-    mainword: "غازسق",
-    word1: "غاسق",
-    coordinats: ["l8", "l9", "l4", "l5", "l10"],
-    prompt: "Данное слово встречается в одной из последних 5 сур, слово из 4 букв",
+    mainword: "عيقد",
+    word1: "عقد",
+    coordinats: ["l8", "l9", "l6", "l7"],
+    prompt: "Данное слово встречается в одной из последних 5 сур, слово из 3 букв",
     img: tanger
 },
 {
-    mainword: "حشبل",
-    word1: "حبل",
+    mainword: "احدص",
+    word1: "احد",
     coordinats: ["l8", "l9", "l6", "l7"],
     prompt: "Данное слово встречается в одной из последних 5 сур, слово из 3 букв",
     img: kairun
 },
 {
-    mainword: "فاتسح",
-    word1: "فتح",
-    coordinats: ["l8", "l9", "l4", "l5", "l10"],
+    mainword: "مسقد",
+    word1: "مسد",
+    coordinats: ["l8", "l9", "l6", "l7"],
     prompt: "Данное слово встречается в одной из последних 5 сур, слово из 3 букв",
     img: alkar
+},
+{
+    mainword: "نقرص",
+    word1: "نصر",
+    coordinats: ["l8", "l9", "l6", "l7"],
+    prompt: "Данное слово встречается в одной из последних 5 сур, слово из 3 букв",
+    img: madina
+},
+{
+    mainword: "دليمن",
+    word1: "دين",
+    coordinats: ["l8", "l9", "l4", "l5", "l10"],
+    prompt: "Данное слово встречается в одной из следующих сур\n:Аль-Маун,Аль-Каусар,Аль-Кафирун, слово из 3 букв",
+    img: ornament
 },
 
 ]
